bf9b7f3db6e60f59d4d59d75857292121da1b27b
unknown <lizzybumpers11@gmail.com>
2023-10-03 09:02:26 -0500

    added js in js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,75 @@
-// Assignment code here
-var passPrompt = prompt('How many characters long?'); {
-  if (passPrompt >= 8 && passPrompt <= 128) {
-  } else (window.alert('Password length must be between 8-128 characters long.'))
-} return;
+var passPrompt
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var special = "!#$%&'()*+,-./:;<=>?@ []^_`{|}~"
 
-window.confirm('Include lowercase letters?');
-window.confirm('Include uppercase letters?');
-window.confirm('Include numeric values?');
-window.confirm('Include special characters?');
+// Assignment code here
+function generatePassword() {
+  passwordPrompt()
+  charactersLowercase()
+  charactersUppercase()
+  charactersNumeric()
+  charactersSpecial()
+}
+
+
+
+
+
+
+function passwordPrompt() {
+  passPrompt = prompt('How many characters long?'); {
+    if (passPrompt >= 8 && passPrompt <= 128) {
+      return;
+    } else {
+      (window.alert('Password length must be between 8-128 characters long.'));
+      passwordPrompt();
+    }
+  }
+}
+
+function charactersLowercase() {
+  lowercase = window.confirm('Include lowercase letters?'); {
+    if (true) {
+      lowercase   
+    } else {
+      false
+    }
+  }
+}
+
+function charactersUppercase() {
+  uppercase = window.confirm('Include uppercase letters?'); {
+    if (true) {
+      uppercase   
+    } else {
+      false
+    }
+  }
+}
+
+function charactersNumeric() {
+  numeric = window.confirm('Include numeric values?'); {
+    if (true) {
+      numeric   
+    } else {
+      false
+    }
+  }
+}
+
+function charactersSpecial() {
+  special = window.confirm('Include special characters?'); {
+    if (true) {
+      special   
+    } else {
+      false
+    }
+  }
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
