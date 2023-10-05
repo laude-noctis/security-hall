f22f2228e37503a6bffb4b013b7bf0635542f662
unknown <lizzybumpers11@gmail.com>
2023-10-04 21:39:10 -0500

    pushed into array but not randomizing characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,49 @@
 var passPrompt
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"]
 var storeCharacters = []
+var retVal = ""
 
 function generatePassword() {
   passwordPrompt()
   var lowercasePrompt = window.confirm('Include lowercase letters?');
-  if(lowercasePrompt) {
-    storeCharacters.push(lowercase)
+  if (lowercasePrompt) {
+    for (let i = 0; i < lowercase.length; ++i) {
+      storeCharacters.push(lowercase[i])
+    }
   }
 
   var uppercasePrompt = window.confirm('Include uppercase letters?');
-  if(uppercasePrompt) {
-    storeCharacters.push(uppercase)
+  if (uppercasePrompt) {
+    for (let i = 0; i < uppercase.length; ++i) {
+      storeCharacters.push(uppercase[i])
+    }
   }
 
   var specialPrompt = window.confirm('Include special characters?');
   if (specialPrompt) {
-    storeCharacters.push(special)
+    for (let i = 0; i < special.length; ++i) {
+      storeCharacters.push(special[i])
+    }
   }
 
   var numericPrompt = window.confirm('Include numeric values?');
   if (numericPrompt) {
-    storeCharacters.push(numeric)
+    for (let i = 0; i < numeric.length; ++i) {
+      storeCharacters.push(numeric[i])
+    }
   }
 
-  for (var i = 0, n = charset.length; i < length; ++i) {
-  retVal += charset.charAt(Math.floor(Math.random() * n));
-}
-  return document.querySelector("#password")
+  for (var n = 0; n = storeCharacters.length; ++n) {
+    // retVal += storeCharacters[n]
+    // console.log(Math.floor(Math.random() * n))
+  }
+  console.log(storeCharacters)
+  console.log(retVal)
+  // this needs to be fixed because we dont want the 0 index
+  return document.querySelector("#password").value = storeCharacters[0]
 }
 
 function passwordPrompt() {
@@ -49,9 +62,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); 
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
- 
+
   passwordText.value = password;
 }
 // Add event listener to generate button
