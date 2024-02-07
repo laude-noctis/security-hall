8e003a1dac16403079d438b4e691a8c4b418d990
unknown <lizzybumpers11@gmail.com>
2024-02-07 10:16:59 -0600

    scoped the storeChracters array to generatePassword function

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"]
-var storeCharacters = []
 
 function generatePassword() {
+  var storeCharacters = []
+
   passwordPrompt()
   var lowercasePrompt = window.confirm('Include lowercase letters?');
   if (lowercasePrompt) {
